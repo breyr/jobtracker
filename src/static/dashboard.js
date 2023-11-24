872c1d1b496feb467b63462d3a978713284c0c23
breyr <breyr2021@gmail.com>
2023-11-24 16:13:53 -0500

    feat: Sorting rows

--- src/static/dashboard.js
+++ src/static/dashboard.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+    // get original rows for default order
+    $originalrows = $('tbody tr');
+
     // Handle click event for the close button
     $('#application-modal #closeappbtn').click(function () {
         $('#application-modal').modal('hide');
@@ -41,6 +44,12 @@ $(document).ready(function () {
                     // remove row from table
                     $operations.forEach(function (op) {
                         $('#' + op['delete']['id']).remove();
+                    });
+                    // remove row from originalrows
+                    $originalrows = $originalrows.filter(function (row) {
+                        return !($operations.some(function (op) {
+                            return op['delete']['id'] == row.id;
+                        }));
                     });
                 },
                 error: function () {
@@ -149,6 +158,9 @@ $(document).ready(function () {
 
                 // hide modal
                 $('#new-application-modal').modal('hide');
+
+                // add new row to originalrows
+                $originalrows.push($newApp[0]);
             },
             error: function () {
                 // show error message
@@ -201,4 +213,43 @@ $(document).on('click', 'tbody tr', function (event) {
 
     // show modal
     $('#application-modal').modal('show');
+
+    // functions for sorting
+    $('#status-sort').click(function () { sortRows($(this)); });
+    $('#company-sort').click(function () { sortRows($(this)); });
+    $('#position-sort').click(function () { sortRows($(this)); });
+    $('#date-sort').click(function () { sortRows($(this)); });
+
 });
+
+function sortRows(col) {
+    $colIdx = col.parent().index();
+    $rows = $('tbody tr');
+    console.log($colIdx);
+    if (col.hasClass('fa-sort')) {
+        // do ascending sort on rows based on column index
+        $sorted = [...$rows].sort(function (a, b) {
+            $aVal = $(a).children().eq($colIdx).text();
+            $bVal = $(b).children().eq($colIdx).text();
+            return $aVal.localeCompare($bVal);
+        });
+        $('tbody').empty();
+        $('tbody').append($sorted);
+        col.removeClass('fa-sort').addClass('fa-sort-up');
+    } else if (col.hasClass('fa-sort-up')) {
+        // do descending sort on rows based on column index
+        col.removeClass('fa-sort-up').addClass('fa-sort-down');
+        $sorted = [...$rows].sort(function (a, b) {
+            $aVal = $(a).children().eq($colIdx).text();
+            $bVal = $(b).children().eq($colIdx).text();
+            return $bVal.localeCompare($aVal);
+        });
+        $('tbody').empty();
+        $('tbody').append($sorted);
+    } else {
+        // replace rows with default order
+        col.removeClass('fa-sort-down').addClass('fa-sort');
+        $('tbody').empty();
+        $('tbody').append($originalrows);
+    }
+}
